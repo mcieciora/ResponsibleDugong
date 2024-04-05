9eb585e9b42cfa998ed0779548c71605e25a630f
mcieciora <mcieciora@nowhere.com>
2024-04-05 14:07:38 +0200

    05042024-1407

--- scripts/scan_jenkins_image.sh
+++ scripts/scan_jenkins_image.sh
@@ -8,10 +8,10 @@ docker run -e DOCKER_SCOUT_HUB_USER="$USERNAME" -e DOCKER_SCOUT_HUB_PASSWORD="$P
 SCAN_RESULT=$?
 if [ "$SCAN_RESULT" -ne 0 ]; then
   echo "Vulnerabilities found. Running recommendations"
-  docker scout recommendations "$TESTED_IMAGE" > scan_jenkins.txt
-  grep 'This image version is up to date' scan_jenkins.txt
+  docker run -e DOCKER_SCOUT_HUB_USER="$USERNAME" -e DOCKER_SCOUT_HUB_PASSWORD="$PASSWORD" -v /var/run/docker.sock:/var/run/docker.sock docker/scout-cli recommendations "$TESTED_IMAGE" > scan_jenkins.txt
+  grep "This image version is up to date" scan_jenkins.txt
   IMAGE_UP_TO_DATE=$?
-  grep 'There are no tag recommendations at this time' scan_jenkins.txt
+  grep "There are no tag recommendations at this time" scan_jenkins.txt
   RECOMMENDATIONS_AVAILABLE=$?
   if [ "$IMAGE_UP_TO_DATE" -eq 0 ] && [ "$RECOMMENDATIONS_AVAILABLE" -eq 0 ] && [ "$RETURN_VALUE" -ne 1 ]; then
     RETURN_VALUE=0
