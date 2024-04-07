3fab815029f98e97ad33536ba1ae6c08eeece01e
mcieciora <mcieciora@nowhere.com>
2024-04-07 13:49:59 +0200

    07042024-1349

--- scripts/test_jenkins_setup.sh
+++ scripts/test_jenkins_setup.sh
@@ -24,7 +24,7 @@ function wait_for_jenkins_instance() {
 
 function generate_crumb_and_token() {
   OUTPUT_FILE="token_data.json"
-  JENKINS_URL="http://localhost:8085"
+  JENKINS_URL="http://test_jenkins_instance:8080"
   JENKINS_USER="$JENKINS_ADMIN_USER"
   JENKINS_PASSWORD="$JENKINS_ADMIN_PASS"
   echo "$JENKINS_USER"
@@ -106,8 +106,7 @@ function test_on_next_jenkins_build_pipeline() {
 }
 
 echo "Launching Jenkins instance..."
-# docker run -d --name test_jenkins_instance --env-file .env_example --network jenkins_network "$DOCKERHUB_REPO":jenkins_image
-docker run -p 8085:8080 -d --name test_jenkins_instance --env-file .env_example jenkins_image
+docker run -d --name test_jenkins_instance --env-file .env_example --network jenkins_network "$DOCKERHUB_REPO":jenkins_image
 echo "Sleeping for 5 seconds before checking boot status..."
 sleep 5
 
