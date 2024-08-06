aaee1d8f1ef6a7f2f522807ed64058f250c2be22
mcieciora <mcieciora@nowhere.com>
2024-08-06 21:30:27 +0200

    06082024-2130

--- scripts/app_health_check.sh
+++ scripts/app_health_check.sh
@@ -3,6 +3,8 @@
 CHECK_TIME=$1
 EXPECTED_VALUE=$2
 
+sh "sed -i 's~8080:8080~8001:8080~g' docker-compose.yml"
+sh "sed -i 's~5432:5432~8001:5432~g' docker-compose.yml"
 
 docker compose up -d
 echo "Sleeping for $CHECK_TIME"
