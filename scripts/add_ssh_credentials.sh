92a16f712179b5998c9e0df6b5a5a2d8b94de868
mcieciora <mcieciora@nowhere.com>
2024-10-06 18:46:47 +0200

    06102024-1846

--- scripts/add_ssh_credentials.sh
+++ scripts/add_ssh_credentials.sh
@@ -2,20 +2,6 @@
 
 set -e
 
-function generate_crumb_and_token() {
-  OUTPUT_FILE="token_data.json"
-  echo "Sending crumb request..."
-  CRUMB=$(curl "$JENKINS_URL/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,%22:%22,//crumb)" --cookie-jar cookies.txt --user "$JENKINS_ADMIN_USER:$JENKINS_PASSWORD")
-  echo "Using crumb to get API token..."
-  TOKEN_DATA=$(curl "$JENKINS_URL/user/$JENKINS_ADMIN_USER/descriptorByName/jenkins.security.ApiTokenProperty/generateNewToken" \
-  --user "$JENKINS_ADMIN_USER:$JENKINS_ADMIN_PASS" --data "newTokenName=jenkins-token" --cookie cookies.txt -H "$CRUMB")
-  echo "$TOKEN_DATA"
-  echo "Saving acquired API token..."
-  echo "$TOKEN_DATA" > "$OUTPUT_FILE"
-  TOKEN=$(jq -r ".data.tokenValue" "$OUTPUT_FILE")
-  echo "$TOKEN"
-}
-
 function add_credentials() {
   echo "Creating SSH credentials"
   curl -X POST "$JENKINS_URL/credentials/store/system/domain/_/createCredentials" --user "$JENKINS_ADMIN_USER:$TOKEN" \
@@ -38,6 +24,4 @@ function add_credentials() {
   }'
 }
 
-
-generate_crumb_and_token
 add_credentials
---
+++ scripts/generate_crumb.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+function generate_crumb_and_token() {
+  OUTPUT_FILE="token_data.json"
+  echo "Sending crumb request..."
+  CRUMB=$(curl "$JENKINS_URL/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,%22:%22,//crumb)" --cookie-jar cookies.txt --user "$JENKINS_ADMIN_USER:$JENKINS_PASSWORD")
+  echo "Using crumb to get API token..."
+  TOKEN_DATA=$(curl "$JENKINS_URL/user/$JENKINS_ADMIN_USER/descriptorByName/jenkins.security.ApiTokenProperty/generateNewToken" \
+  --user "$JENKINS_ADMIN_USER:$JENKINS_ADMIN_PASS" --data "newTokenName=jenkins-token" --cookie cookies.txt -H "$CRUMB")
+  echo "$TOKEN_DATA"
+  echo "Saving acquired API token..."
+  echo "$TOKEN_DATA" > "$OUTPUT_FILE"
+  TOKEN=$(jq -r ".data.tokenValue" "$OUTPUT_FILE")
+  echo "$TOKEN" > "token.sec"
+}
+
+generate_crumb_and_token
