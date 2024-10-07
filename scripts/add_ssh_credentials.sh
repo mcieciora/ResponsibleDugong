b9a09e41a68a64ff81aad15fc1d0b0f2575afd3f
mcieciora <mcieciora@nowhere.com>
2024-10-07 16:36:35 +0200

    07102024-1636

--- scripts/add_ssh_credentials.sh
+++ scripts/add_ssh_credentials.sh
@@ -4,19 +4,17 @@ set -e
 
 function add_ssh_credentials() {
   echo "Creating SSH credentials"
-  echo "$TOKEN"
-  echo "$HOME_PATH"
   curl -X POST "$JENKINS_URL/credentials/store/system/domain/_/createCredentials" --user "$JENKINS_ADMIN_USER:$TOKEN" \
   --data-urlencode 'json={
      "":"2",
      "credentials":{
         "scope":"GLOBAL",
-        "id":"$1",
+        "id":"$(echo $AGENT_NAME)",
         "description":"",
         "username":"jenkins_server",
         "privateKeySource":{
            "value":"0",
-           "privateKey":"$(cat /$HOME_PATH/.ssh/id_ed25519)",
+           "privateKey":"$(cat ${env.SSH_PATH}/id_ed25519)",
            "stapler-class":"com.cloudbees.jenkins.plugins.sshcredentials.impl.BasicSSHUserPrivateKey$DirectEntryPrivateKeySource",
            "\$class":"com.cloudbees.jenkins.plugins.sshcredentials.impl.BasicSSHUserPrivateKey$DirectEntryPrivateKeySource"
         },
