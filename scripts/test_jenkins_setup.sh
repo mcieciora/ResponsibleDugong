f5bab15720c70dd9742a4676eee860e3c94c00a7
mcieciora <michalcieciora96@gmail.com>
2024-11-27 10:00:28 +0100

    27112024-0959

--- scripts/test_jenkins_setup.sh
+++ scripts/test_jenkins_setup.sh
@@ -27,9 +27,6 @@ function generate_crumb_and_token() {
   JENKINS_USER="$JENKINS_ADMIN_USER"
   JENKINS_PASSWORD="$JENKINS_ADMIN_PASS"
   echo "Sending crumb request..."
-
-  CRUMB=$(curl "http://test_jenkins_instance:8080/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,%22:%22,//crumb)" --cookie-jar cookies.txt --user "$JENKINS_USER:$JENKINS_PASSWORD")
-
   CRUMB=$(curl "$JENKINS_URL/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,%22:%22,//crumb)" --cookie-jar cookies.txt --user "$JENKINS_USER:$JENKINS_PASSWORD")
   echo "Using crumb to get API token..."
   TOKEN_DATA=$(curl "$JENKINS_URL/user/$JENKINS_USER/descriptorByName/jenkins.security.ApiTokenProperty/generateNewToken" \
