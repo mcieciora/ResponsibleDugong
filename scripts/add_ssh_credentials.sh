747bc0428c33a61fe61d8ce90a89dcba1c3f053e
mcieciora <michalcieciora96@gmail.com>
2025-03-20 11:25:59 +0100

    20032025-1125

--- scripts/add_ssh_credentials.sh
+++ scripts/add_ssh_credentials.sh
@@ -2,27 +2,28 @@
 
 set -e
 
-function add_ssh_credentials() {
-  echo "Creating SSH credentials"
-  ID_CONTENT=$(cat "$SSH_PATH"/id_ed25519)
-  echo "json={
-     '':'2',
-     'credentials':{
-        'scope':'GLOBAL',
-        'id':'agent_'$AGENT_NAME,
-        'description':'',
-        'username':'jenkins_server',
-        'privateKeySource':{
-           'value':'0',
-           'privateKey':$ID_CONTENT,
-           'stapler-class':'com.cloudbees.jenkins.plugins.sshcredentials.impl.BasicSSHUserPrivateKey\$DirectEntryPrivateKeySource',
-           '\$class':'com.cloudbees.jenkins.plugins.sshcredentials.impl.BasicSSHUserPrivateKey\$DirectEntryPrivateKeySource'
+echo "Creating SSH credentials"
+ID_CONTENT=$(cat "$SSH_PATH"/id_ed25519)
+JSON_CONTENT=$(cat <<EOF
+  {
+     "":"2",
+     "credentials":{
+        "scope":"GLOBAL",
+        "id":"agent_$AGENT_NAME",
+        "description":"",
+        "username":"jenkins_server",
+        "privateKeySource":{
+           "value":"0",
+           "privateKey":"$ID_CONTENT",
+           "stapler-class":"com.cloudbees.jenkins.plugins.sshcredentials.impl.BasicSSHUserPrivateKey\$DirectEntryPrivateKeySource",
+           "\$class":"com.cloudbees.jenkins.plugins.sshcredentials.impl.BasicSSHUserPrivateKey\$DirectEntryPrivateKeySource"
         },
-        'passphrase':'',
-        '\$class':'com.cloudbees.jenkins.plugins.sshcredentials.impl.BasicSSHUserPrivateKey'
+        "passphrase":"",
+        "\$class":"com.cloudbees.jenkins.plugins.sshcredentials.impl.BasicSSHUserPrivateKey"
      }
-  }" > curl_data.json
-  curl -X POST "$JENKINS_URL/credentials/store/system/domain/_/createCredentials" --user "$JENKINS_ADMIN_USER:$TOKEN" -d @curl_data.json
-}
-
-add_ssh_credentials
+  }
+EOF
+)
+echo "$JSON_CONTENT" > curl_data.json
+cat curl_data.json
+curl -X POST "$JENKINS_URL/credentials/store/system/domain/_/createCredentials" --user "$JENKINS_ADMIN_USER:$TOKEN" -d @curl_data.json
