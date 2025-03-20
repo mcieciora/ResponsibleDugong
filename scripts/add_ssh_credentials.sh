61f85bfab5c150550ab411974824a4ad020cff5b
mcieciora <michalcieciora96@gmail.com>
2025-03-20 12:21:15 +0100

    20032025-1218

--- scripts/add_ssh_credentials.sh
+++ scripts/add_ssh_credentials.sh
@@ -3,24 +3,25 @@
 set -e
 
 echo "Creating SSH credentials"
+ID_CONTENT=$(cat "$SSH_PATH"/id_ed25519)
 JSON_CONTENT=$(cat <<EOF
-  json={
-     "":"2",
-     "credentials":{
-        "scope":"GLOBAL",
-        "id":"agent_$AGENT_NAME",
-        "description":"",
-        "username":"jenkins_server",
-        "privateKeySource":{
-           "value":"0",
-           "privateKey":$(cat "$SSH_PATH"/id_ed25519),
-           "stapler-class":"com.cloudbees.jenkins.plugins.sshcredentials.impl.BasicSSHUserPrivateKey\$DirectEntryPrivateKeySource",
-           "\$class":"com.cloudbees.jenkins.plugins.sshcredentials.impl.BasicSSHUserPrivateKey\$DirectEntryPrivateKeySource"
-        },
-        "passphrase":"",
-        "\$class":"com.cloudbees.jenkins.plugins.sshcredentials.impl.BasicSSHUserPrivateKey"
-     }
-  }
+json={
+   "":"2",
+   "credentials":{
+      "scope":"GLOBAL",
+      "id":"agent_$AGENT_NAME",
+      "description":"",
+      "username":"jenkins_server",
+      "privateKeySource":{
+         "value":"0",
+         "privateKey":"$ID_CONTENT",
+         "stapler-class":"com.cloudbees.jenkins.plugins.sshcredentials.impl.BasicSSHUserPrivateKey\$DirectEntryPrivateKeySource",
+         "\$class":"com.cloudbees.jenkins.plugins.sshcredentials.impl.BasicSSHUserPrivateKey\$DirectEntryPrivateKeySource"
+      },
+      "passphrase":"",
+      "\$class":"com.cloudbees.jenkins.plugins.sshcredentials.impl.BasicSSHUserPrivateKey"
+   }
+}
 EOF
 )
 echo 'json={
