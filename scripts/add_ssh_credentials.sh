cbe55b6d5a9e7dde49d5d4acd851e432a3659ddd
mcieciora <michalcieciora96@gmail.com>
2025-03-20 12:34:40 +0100

    20032025-1234

--- scripts/add_ssh_credentials.sh
+++ scripts/add_ssh_credentials.sh
@@ -24,22 +24,5 @@ json={
 }
 EOF
 )
-echo 'json={
-   "":"2",
-   "credentials":{
-      "scope":"GLOBAL",
-      "id":"agent_'$AGENT_NAME'",
-      "description":"",
-      "username":"jenkins_server",
-      "privateKeySource":{
-         "value":"0",
-         "privateKey":"'$(cat $SSH_PATH/id_ed25519)'",
-         "stapler-class":"com.cloudbees.jenkins.plugins.sshcredentials.impl.BasicSSHUserPrivateKey$DirectEntryPrivateKeySource",
-         "\$class":"com.cloudbees.jenkins.plugins.sshcredentials.impl.BasicSSHUserPrivateKey$DirectEntryPrivateKeySource"
-      },
-      "passphrase":"",
-      "$class":"com.cloudbees.jenkins.plugins.sshcredentials.impl.BasicSSHUserPrivateKey"
-   }
-}' > original_json.json
 echo "$JSON_CONTENT" > curl_data.json
 curl -X POST "$JENKINS_URL/credentials/store/system/domain/_/createCredentials" --user "$JENKINS_ADMIN_USER:$TOKEN" --data-binary @curl_data.json
